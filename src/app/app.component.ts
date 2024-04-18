7ea6893eaa2040c5426e3a1d584abaf4a893268f
manojvictor <fmanojvictor@gmail.com>
2024-04-18 16:29:55 +0530

    Get survey data from json and send the user input data to BE changes - Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as exampleData from '../assets/we-craft DemoSurvey V1.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'we-craft';
+  data: any;
+  selectedValue = "Yes";
+
+  constructor() {
+    console.log(exampleData);
+    this.data = exampleData;
+  }
+
+  toggleSelection(e: any, items: any, i: number, response: string[]): void {
+    if (e.target.checked && !response.some(x => x == items[i])) {
+      response.push(items[i]);
+    } else if (!e.target.checked) {
+      response.splice(response.indexOf(items[i]), 1);
+    }
+  }
+
+  onSubmit() {
+    console.log(this.data.default);
+  }
 }
